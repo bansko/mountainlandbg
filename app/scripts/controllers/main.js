d99435be0a5b5f10765cda92b39d04cbf56f2028
Rob Thijssen <rthijssen@gmail.com>
2018-08-26 12:26:40 +0300

    yo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mountainlandbgApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mountainlandbgApp
+ */
+angular.module('mountainlandbgApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
